0b95f7d8a9f9bcd1d1b86c9a462bfff1b786158f
Raine Virta <raine.virta@gmail.com>
2012-08-21 16:01:56 +0300

    deploy: don't have to remove .git/, rsync excludes it

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ set -x verbose #echo on
 coffee -c triangles.coffee
 # cp -R ../triangles ~/Dropbox/Public
 rsync --exclude '.git' -vva ../triangles /Users/raine/Dropbox/Public
-rm -rf ~/Dropbox/Public/triangles/.git
 open ~/Dropbox/Public/triangles
