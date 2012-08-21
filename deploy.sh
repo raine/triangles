d8ae896883d9a51cb8f579716d4d9840db0c411f
Raine Virta <raine.virta@gmail.com>
2012-08-21 16:06:12 +0300

    deploy: exclude .git*, deploy.sh and *.coffee

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,9 @@ set -x verbose #echo on
 
 coffee -c triangles.coffee
 # cp -R ../triangles ~/Dropbox/Public
-rsync --exclude '.git' -vva ../triangles /Users/raine/Dropbox/Public
+rsync --delete-excluded \
+  --exclude '.git*' \
+  --exclude 'deploy.sh' \
+  --exclude '*.coffee' \
+  -vva ../triangles /Users/raine/Dropbox/Public
 open ~/Dropbox/Public/triangles
